8eae306e0aa739ee5e63736e129d6ea0fc7ea767
Martin Becker <becker@informatik.uni-wuerzburg.de>
2017-10-03 14:19:52 +0200

    Update meta data for PyPi

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ import setuptools as st
 st.setup(
     name='pytrails',
     version='0.0.2',
-    packages=st.find_packages()
+    packages=st.find_packages(),
+
+    # metadata for upload to PyPI
+    author="Martin Becker",
+    author_email="becker@informatik.uni-wuerzburg.de",
+    description="A collection of Bayesian methods for hypothesis comparison on sequential data.",
+    keywords="bayesian model comparison bayes factor Markov model sequence sequential data",
+    url="https://github.com/mgbckr/pytrails",
 )
